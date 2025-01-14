d89a0e7a908f7e053155a31005b0881c59f6fd47
Riccardo <RiccardoL_02@outlook.com>
2025-01-14 11:16:05 +0100

    CS scripts

---
+++ Scripts/Plantes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planets : MonoBehaviour {
+    void start() {
+
+    }
+
+    void update(){
+
+    }
+}
